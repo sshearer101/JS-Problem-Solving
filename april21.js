c4a30c0ff066a9ba3377b805cbac1160eab8b382
Sam Shearer <sshearer101@gmail.com>
2022-04-21 19:15:32 -0600

    third

--- april21.js
+++ april21.js
@@ -40,3 +40,31 @@ function nameShuffler(str){
     
   }
   
+
+
+
+//   A Narcissistic Number is a number of length n in which the sum of its digits to the power of n is equal 
+//   to the original number. If this seems confusing, refer to the example below.
+// Ex: 153, where n = 3 (number of digits in 153)
+// 13 + 53 + 33 = 153
+
+// Write a method is_narcissistic(i) (in Haskell: isNarcissistic :: Integer -> Bool) which returns whether or not i is a Narcissistic Number.
+
+  function isNarcissistic(n){
+    const newString = n.toString().split('')
+    
+    let count = 0
+    
+    for (let i = 0; i< newString.length; i++){
+      
+      count += ((newString[i]) ** (newString.length))
+      
+      }
+    
+  if(count === n){
+    return true;
+  }
+    else{
+      return false
+    }
+  }
