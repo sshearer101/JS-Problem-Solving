e1ced00c7c90f5403307a9e534c231106372f0f9
Sam Shearer <sshearer101@gmail.com>
2022-04-21 18:15:14 -0600

    second

--- april21.js
+++ april21.js
@@ -26,3 +26,17 @@ function humanYearsCatYearsDogYears(humanYears) {
     newArray.push(humanYears, cat, dog)
   }
 }
+
+
+
+// https://www.codewars.com/kata/559ac78160f0be07c200005a/train/javascript
+
+// Write a function that returns a string in which firstname is swapped with last name.
+
+
+function nameShuffler(str){
+  
+    return str.split(' ').reverse().join(' ')
+    
+  }
+  
