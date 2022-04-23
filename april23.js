27808e13a49a90a7aa59f913c63a7413a0064ccd
Sam Shearer <sshearer101@gmail.com>
2022-04-23 15:16:07 -0600

    second

--- april23.js
+++ april23.js
@@ -53,3 +53,22 @@ function magNumber(info){
   //   console.log(weapons["PT92"])
     return Math.ceil((info[1] * 3) / weapons[info[0]])
   }
+
+
+
+//   Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the 
+//   outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
+
+
+// ("1", "22") --> "1221"
+// ("22", "1") --> "1221"
+
+
+  function solution(a, b){
+    if (b.length > a.length) {
+     return a + b + a
+    } else if (a.length > b.length) {
+     return b + a + b
+    } 
+    
+   }
