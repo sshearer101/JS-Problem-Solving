77b4eca108324a36ea20a29462b137df2139dd3b
Sam Shearer <sshearer101@gmail.com>
2022-04-23 15:33:34 -0600

    alternative

--- april23.js
+++ april23.js
@@ -100,3 +100,9 @@ function magNumber(info){
     return newArray
 
 }
+
+// Alternative 
+
+function openOrSenior(data){
+    return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
+  }
