22b06c7012bd1cb4edcbe22e4b7e62065c65f9c8
Sam Shearer <sshearer101@gmail.com>
2022-05-01 15:45:42 -0600

    cuboid

--- may1.js
+++ may1.js
@@ -54,5 +54,14 @@ function stringMerge(string1, string2, letter){
    function stringMerge(string1, string2, letter) {
     return string1.slice(0, string1.indexOf(letter)) + string2.slice(string2.indexOf(letter));
     
-  
+  }
+
+
+//   Bob needs a fast way to calculate the volume of a cuboid with three values: length, width and the height of the cuboid. Write a function to help Bob with this calculation.
+
+
+  class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+     return (length * width * height)
+  }
   }
