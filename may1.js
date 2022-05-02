90be6f45eb250961cb138fd8dff9da0b97a36ce3
Sam Shearer <sshearer101@gmail.com>
2022-05-02 13:33:13 -0600

    description

--- may1.js
+++ may1.js
@@ -88,7 +88,7 @@ function stringMerge(string1, string2, letter){
 
 
 
-  
+
 //   Your task is to find the first element of an array that is not consecutive.
 
 //   By not consecutive we mean not exactly 1 larger than the previous element of the array.
@@ -110,3 +110,22 @@ function stringMerge(string1, string2, letter){
    }
     return null
   }
+
+
+
+//   Write a function:
+
+// function solution(A);
+
+// that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+
+// For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+
+// Given A = [1, 2, 3], the function should return 4.
+
+// Given A = [−1, −3], the function should return 1.
+
+// Write an efficient algorithm for the following assumptions:
+
+// N is an integer within the range [1..100,000];
+// each element of array A is an integer within the range [−1,000,000..1,000,000].
