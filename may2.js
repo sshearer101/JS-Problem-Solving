3c266361d0aef4cda41c00bd505638e9fb799192
Sam Shearer <sshearer101@gmail.com>
2022-05-02 13:47:50 -0600

    task 2

--- may2.js
+++ may2.js
@@ -28,3 +28,17 @@ function solution(A) {
     }
     return smallest
 }
+
+
+
+function solution(A, K) {
+    var n = A.length;
+    for (var i = 0; i < n - 1; i++) {
+        if((A[i] != A[i+1]) &&  (A[i] + 1) != A[i+1])
+            return false;
+    }
+    if (A[0] != 1 || A[n - 1] != K)
+        return false;
+    else
+        return true;
+}
