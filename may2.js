0de7d76227bfdd88778412a34526c4f40386319d
Sam Shearer <sshearer101@gmail.com>
2022-05-02 17:29:37 -0600

    bingo

--- may2.js
+++ may2.js
@@ -130,3 +130,24 @@ function solution(nums){
       return a - b
     });
   }
+
+
+
+//   For this game of BINGO, you will receive a single array of 10 numbers from 1 to 26 as an input. Duplicate numbers within the array are possible.
+
+// Each number corresponds to their alphabetical order letter (e.g. 1 = A. 2 = B, etc). Write a function where you will win the game if your numbers can spell "BINGO". They do not need to be in the right order in the input array). Otherwise you will lose. Your outputs should be "WIN" or "LOSE" respectively.
+  
+function bingo(a) {
+    let bingo = [2, 7, 9, 14, 15]
+    let each = (a.map((x) => x))
+    console.log(a)
+    console.log(each)
+  
+    for (let i = 0; i<a.length; i++){
+      if (a.includes(2) && a.includes(7) && a.includes(9) && a.includes(14) && a.includes(15)){
+    return "WIN"
+  } else{
+    return "LOSE"
+  }
+    }
+}
