3d854a123faec9bf7852f865e9f12149f2521fba
Sam Shearer <sshearer101@gmail.com>
2022-05-04 18:06:22 -0600

    alternative

--- may4.js
+++ may4.js
@@ -77,3 +77,9 @@ function giveChange(amount) {
     }
     return arr;
   }
+
+
+  //alterntive 
+
+  const giveChange = amount => [100, 50, 20, 10, 5, 1].map(a => 
+    ([a, amount] = [Math.floor(amount/a), amount % a])[0]).reverse();
