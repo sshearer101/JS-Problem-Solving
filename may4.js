d53ee0b68c2df96dcc9ff574dba2aa40ff3cf9f0
Sam Shearer <sshearer101@gmail.com>
2022-05-04 18:10:02 -0600

    math

--- may4.js
+++ may4.js
@@ -83,3 +83,31 @@ function giveChange(amount) {
 
   const giveChange = amount => [100, 50, 20, 10, 5, 1].map(a => 
     ([a, amount] = [Math.floor(amount/a), amount % a])[0]).reverse();
+
+
+
+//create functions of math
+
+    function add(a,b){
+        return a+b
+    }
+    
+    function divide(a,b){
+        return a/b
+    }
+    
+    function multiply(a,b){
+        return a*b
+    }
+    
+    function mod(a,b){
+        return a % b
+    }
+       
+    function exponent(a,b){
+        return a ** b
+    }
+        
+    function subt(a,b){
+        return a - b
+    }
