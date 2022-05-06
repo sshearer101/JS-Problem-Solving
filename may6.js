eee76c10c74de0946f88fa2579e7912f24fb6d7a
Sam Shearer <sshearer101@gmail.com>
2022-05-06 15:31:31 -0600

    compass

--- may6.js
+++ may6.js
@@ -44,3 +44,23 @@ function horMirror(str) {
 function oper(fct, s) {
     return fct(s)
 }
+
+
+
+
+// You have an 8-wind compass, like this:
+
+// You receive the direction you are facing (one of the 8 directions: N, NE, E, SE, S, SW, W, NW) and a certain degree to turn (a multiple of 45, between -1080 and 1080); positive means clockwise, and negative means counter-clockwise.
+
+// Return the direction you will face after the turn.
+
+// Examples
+// "S",  180  -->  "N"
+// "SE", -45  -->  "E"
+// "W",  495  -->  "NE"
+
+
+function direction(facing, turn){
+    const arr = "N NE E SE S SW W NW".split(" ");
+    return arr[(arr.indexOf(facing) + Math.floor(turn / 45) + 1080) % 8];
+  }
