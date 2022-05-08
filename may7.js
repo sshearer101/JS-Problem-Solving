28315dbb162e1fe03edc801ad18c12c6bee9ea7c
Sam Shearer <sshearer101@gmail.com>
2022-05-08 16:06:52 -0600

    jaden

--- may7.js
+++ may7.js
@@ -2,13 +2,14 @@
 
 // Bob needs a fast way to calculate the volume of a cuboid with three values: length, width and the height of the cuboid. Write a function to help Bob with this calculation.
 
-const Kata = {
-    getVolumeOfCuboid : (length, width, height) => length * width * height
-  }
+// const Kata = {
+//     getVolumeOfCuboid : (length, width, height) => length * width * height
+//   }
 
-  class Kata {
-    static getVolumeOfCuboid(length, width, height) {
-      return length * width * height
-   }
-   }
+//   class Kata {
+//     static getVolumeOfCuboid(length, width, height) {
+//       return length * width * height
+//    }
+// }
   
+
