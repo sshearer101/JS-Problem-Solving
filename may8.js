cd64134820c271ba8c93d93bf424cd0402904557
Sam Shearer <sshearer101@gmail.com>
2022-05-08 16:09:50 -0600

    alternate

--- may8.js
+++ may8.js
@@ -11,4 +11,14 @@ String.prototype.toJadenCase = function () {
     return this.split(" ").map(function(word){
         return word.charAt(0).toUpperCase() + word.slice(1)}).join(' ')
       }
-   
+ 
+      
+      String.prototype.toJadenCase = function () {
+        var words = this.split(' ');
+      
+        for(var i = 0, wordsLen = words.length; i < wordsLen; i++) {
+          words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+        }
+      
+        return words.join(' ');
+      };
