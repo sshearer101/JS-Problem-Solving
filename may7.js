2b8109a9397692a92a8e9e5e91dbbb6a3ff395ab
Sam Shearer <sshearer101@gmail.com>
2022-05-07 22:01:39 -0600

    cuboid

--- may7.js
+++ may7.js
@@ -5,3 +5,10 @@
 const Kata = {
     getVolumeOfCuboid : (length, width, height) => length * width * height
   }
+
+  class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+      return length * width * height
+   }
+   }
+  
