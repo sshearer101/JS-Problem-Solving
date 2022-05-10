e0db5f998715d6d133af199d21cec31096240ba0
Sam Shearer <sshearer101@gmail.com>
2022-05-10 16:32:23 -0600

    pipes

--- may10.js
+++ may10.js
@@ -60,3 +60,32 @@ function wrap(value) {
   }
   
   
+//   Issue
+//   Looks like some hoodlum plumber and his brother has been running around and damaging your stages again.
+  
+//   The pipes connecting your level's stages together need to be fixed before you receive any more complaints.
+  
+//   Pipes list is correct when each pipe after the first index is greater (+1) than the previous one, and that there is no duplicates.
+  
+//   Task
+//   Given the a list of numbers, return a fixed list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
+  
+//   Example
+//   Input: 1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
+
+  function pipeFix(numbers){
+  
+    const firstNum = numbers[0]
+      const array = []
+  
+    const lastNum = numbers[numbers.length-1]
+    
+    const range = lastNum - firstNum
+  
+    for (let i = 0; i <= range; i++){
+      if(numbers[0] + i <= lastNum){
+          array.push(numbers[0] + i)
+      }
+    }
+  return (array)
+  }
