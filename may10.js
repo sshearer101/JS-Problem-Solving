2c20ee69ce8e07192c4dd4f5af2bd820db185d12
Sam Shearer <sshearer101@gmail.com>
2022-05-10 16:33:25 -0600

    pipe

--- may10.js
+++ may10.js
@@ -89,3 +89,17 @@ function wrap(value) {
     }
   return (array)
   }
+
+  //alternative
+
+  function pipeFix(numbers){
+    var first = numbers[0];
+    var last = numbers[numbers.length-1];
+    
+    var arr = [];
+    for(var i = first; i <= last; i++) {
+      arr.push(i);
+    }
+    return arr;
+  }
+  
