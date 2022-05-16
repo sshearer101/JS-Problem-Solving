beedc952c591d776a9244781797f334322f86eb5
Sam Shearer <sshearer101@gmail.com>
2022-05-16 16:14:50 -0600

    clothes

--- may16.js
+++ may16.js
@@ -71,3 +71,12 @@ function cannonsReady(gunners){
           return Number((water * (1.1 **(clothes - load))).toFixed(2))
         }
       }
+
+
+      //alternative 
+
+      function howMuchWater(water, load, clothes){
+        if (clothes/load > 2) return 'Too much clothes'
+        if (clothes < load) return 'Not enough clothes'
+        return +(water * Math.pow(1.1, (clothes - load))).toFixed(2)
+      }
