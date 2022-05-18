cd09516e3bf90bf953aac2e822e1243e2c85b16f
Sam Shearer <85294886+sshearer101@users.noreply.github.com>
2022-05-18 10:54:39 -0600

    traffic

--- may18.js
+++ may18.js
@@ -3,3 +3,15 @@
 // Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
 
 // For example, update_light('green') should return 'yellow'.
+
+function updateLight(current) {
+  
+    if(current === "green"){
+      return "yellow"
+    } else if (current === "yellow"){
+      return "red"
+    } else{
+      return "green"
+    }
+    
+    }
