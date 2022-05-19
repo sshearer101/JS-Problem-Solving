96701d530ec0159b6247cccafe4e1af314b09bb0
Sam Shearer <85294886+sshearer101@users.noreply.github.com>
2022-05-19 16:26:27 -0600

    games

--- may19.js
+++ may19.js
@@ -21,3 +21,40 @@ function warnTheSheep(queue) {
     const position = queue.reverse().indexOf('wolf');
     return position === 0 ? 'Pls go away and stop eating my sheep' : `Oi! Sheep number ${ position }! You are about to be eaten by a wolf!`;
   }
+
+
+
+  
+//   Our football team finished the championship. The result of each match look like "x:y". Results of all matches are recorded in the collection.
+
+//   For example: ["3:1", "2:2", "0:1", ...]
+  
+//   Write a function that takes such collection and counts the points of our team in the championship. Rules for counting points for each match:
+  
+//   if x > y: 3 points
+//   if x < y: 0 point
+//   if x = y: 1 point
+//   Notes:
+  
+//   there are 10 matches in the championship
+//   0 <= x <= 4
+//   0 <= y <= 4
+  
+
+
+  function points(games) {
+  
+    let count = 0
+    
+    for (let x = 0; x<games.length; x++){
+      const point = games[x].split('')
+      console.log(point)
+      if (point[0] > point[2]){
+        count = count + 3
+      } else if (point[0] === point[2]){
+        count = count + 1
+      } 
+    }
+        return count
+  
+  }
